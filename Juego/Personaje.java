6d6d5d277a40223bdc90c0ac24b0214900a8b82f
2003meli <112879521+2003meli@users.noreply.github.com>
2022-09-27 10:47:45 -0500

    Add files via upload

---
+++ Juego/Personaje.java
@@ -0,0 +1,25 @@
+public abstract class Personaje {
+      int salud;
+       int fuerza;
+       int poder;
+     public  int getSalud() {
+        return salud;
+    }
+    public  void setSalud(int salud1) {
+        salud = salud1;
+    }
+    public int getFuerza() {
+        return fuerza;
+    }
+    public  void setFuerza(int fuerza1) {
+        fuerza = fuerza1;
+    }
+    public int getPoder() {
+        return poder;
+    }
+    public  void setPoder(int poder1) {
+        poder=poder1;
+    }
+    
+    
+}
---
+++ Juego/Rugal.java
@@ -0,0 +1,15 @@
+public class Rugal extends Personaje{
+    public Rugal(){
+      salud=1000;
+      fuerza=50;
+     poder=10;
+    }
+    public static void detalles(){
+        System.out.println("Rugal tiene");
+        System.out.println("salud: 1000");
+        System.out.println("fuerza: 50");
+        System.out.println("poder: 10");
+        System.out.println();
+    }
+}
+ 
